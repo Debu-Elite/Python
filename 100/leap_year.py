0148dc56dac99bc1ef6b03f699a0300583581d61
debu <gautamdevansh183@gmail.com>
2026-05-09 15:22:09 +0530

    find a leap year or not
    Signed-off-by: debu <gautamdevansh183@gmail.com>

---
+++ 100/leap_year.py
@@ -0,0 +1,18 @@
+def find_lp_year(num):
+    print("leap_year" if (num%4==0 and num%100 !=0 or num%400==0) else "not leap year")
+
+
+
+
+
+number =int(input("enter the year"))
+find_lp_year(number)
+
+
+
+
+
+
+
+    
+          
